8a36a806ccb57250d1376eacdbe727b4a6dd0553
Ben Gale <ben@bengale.io>
2017-05-07 17:25:34 +0100

    Update Packages

--- src/Authentication/AuthenticationProvider.js
+++ src/Authentication/AuthenticationProvider.js
@@ -1,4 +1,5 @@
-import { Component, Children, PropTypes } from 'react';
+import { Component, Children } from 'react';
+import PropTypes from 'prop-types';
 
 class AuthenticationProvider extends Component {
   getChildContext() {
--- src/Examples/Shared/ContentArea/index.js
+++ src/Examples/Shared/ContentArea/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const ContentArea = ({ contentOptions, selectedContentName }) => {
   const activeContent = contentOptions.find(
